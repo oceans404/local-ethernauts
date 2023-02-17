023614273b53df335afc055fb8f1808a340fab57
Steph <steph@oceans404.com>
2023-02-17 13:08:07 -0800

    reentrance progress

--- contracts/6. Reentrance/AttackingReentrance.sol
+++ contracts/6. Reentrance/AttackingReentrance.sol
@@ -2,6 +2,12 @@
 pragma solidity ^0.8.9;
 import "./Reentrance.sol";
 
+interface ReentranceInterface {
+    function donate(address _to) external payable;
+    // function balanceOf(address _who) public view returns (uint256 balance);
+    function withdraw(uint _amount) external;
+}
+
 contract AttackingReentrance {
     address payable public contractAddress;
 
@@ -11,5 +17,6 @@ contract AttackingReentrance {
 
     function hackContract() external {
         // Code me!
+        ReentranceInterface victimContract = ReentranceInterface(contractAddress);
     }
 }
